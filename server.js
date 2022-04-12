3276737f6e289fe6d1f55068f4fd8cf0b3e35adb
Mats Haugland Johansen <matshj18@uia.no>
2022-04-12 12:56:52 +0200

    added bcrypt password hashing to login/register

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const   express = require('express');
         cors    = require('cors');
         mysql   = require('mysql2');
+        bcrypt  = require('bcryptjs');
 
 // const API_PORT = process.env.PORT || 5000;
 
 const app = express();
+const saltRounds = 9
 
 app.use(express.json());
 app.use(cors());
@@ -21,13 +23,19 @@ app.post("/register", (req, res) => {
     const email = req.body.email
     const password = req.body.password
 
-    db.query(
-        "INSERT INTO Users(Username, Email, Passwords) VALUES(?,?,?)",
-        [username, email, password],
-        (err, result) => {
-            console.log(err);
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if(err) {
+            console.log(err)
         }
-    );
+        db.query(
+            "INSERT INTO Users(Username, Email, Passwords) VALUES(?,?,?)",
+            [username, email, hash],
+            (err, result) => {
+                console.log(err);
+                console.log(hash)
+            }
+        );
+    })
 })
 
 app.post("/login", (req, res) => {
@@ -36,8 +44,8 @@ app.post("/login", (req, res) => {
     const password = req.body.password
 
     db.query(
-        "SELECT * FROM Users WHERE Username=? AND Email=? AND Passwords=?",
-        [username, email, password],
+        "SELECT * FROM Users WHERE Username=? AND Email=?",
+        [username, email],
         (err, result) => {
             if(err) {
                 res.send({err: err})
@@ -45,11 +53,19 @@ app.post("/login", (req, res) => {
             }
 
             if(result.length > 0){
-                res.send(result)
-                console.log(result)
+                bcrypt.compare(password, result[0].Passwords, (err, response) => {
+                    if (response) {
+                        res.send(result)
+                        console.log(result)
+                    }
+                    else {
+                        res.send({message: "Wrong userinfo combination!"})
+                        console.log(password)
+                    }
+                })
             }
             else{
-                res.send({message: "Wrong userinfo combination"})
+                res.send({message: "User doesn't exist"})
             }
         }
     );
